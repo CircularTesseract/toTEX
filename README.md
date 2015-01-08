2bb43b1e0b7e0b2ceeff2ef83936e3f42530ce84
Alexandre Laporte <alexandre.laporte@mail.mcgill.ca>
2015-01-08 12:21:41 -0500

    Added the setup and creation of conf and edited readme

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # toTEX
 A python script to convert markdown notes with a specific syntax to pdf using LaTeX
+It provides functionality akin to the program 'make'. Once the initial config phase is run
+the generated .conf file can be edited manually. 
+*NOTE* This program is designed to run on Unix based systems, no guarantees are made with regard to other
+operatings systems (e.g windows)
---
+++ setup.py
@@ -0,0 +1,34 @@
+import os
+import sys
+
+#Note it is assumed that the structure of the
+#toTEX folders are not modified
+if not os.path.isfile(os.getcwd() + "/toTEX.conf"):
+    f=open(os.getcwd()+ "/toTEX.conf", 'w')
+    #we know the file did not exist previously thus it is safe to print **1 to it as this will be the first entry
+    f.write("**1\n")
+    print "please enter the root directory which contains the project files"
+    print "Note only absolute paths are accepted:"
+    data = sys.stdin.readline()
+    if not os.path.isdir(''.join(data).rstrip('\n')):
+        #user has provided a path that does not exist or is not vissible
+        print "invalid path provided, exiting now"
+        #clean up the conf file fragment
+        os.remove(os.getcwd() + "/toTEX.conf")
+        sys.exit()
+    else:
+        #the file provided is alright
+        f.write("#path:"+ ''.join(data))
+
+else:
+    if os.path.getsize(os.getcwd()+"/toTEX.conf")==0:
+            #if the file exists and is empty something is wrong
+            print ".conf file exists but is empty! cleaning up and exiting"
+            os.remove(os.getcwd()+"/toTEX.conf")
+            sys.exit()
+    else:
+            #no reason to be running setup utility with no flags
+            print ".conf seems in order, exiting"
+            sys.exit()
+
+
